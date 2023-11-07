9e2c99528e6c30fbba4acac178c3ba029913a98a
Mobb-dev <94089762+Mobb-dev@users.noreply.github.com>
2023-11-07 08:49:06 -0500

    test

--- src/main/java/SqlInjectionLesson.java
+++ src/main/java/SqlInjectionLesson.java
@@ -20,6 +20,7 @@
  * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
  */
 
+ 
 package org.owasp.webgoat.lessons.sqlinjection.introduction;
 
 import jakarta.servlet.http.HttpServletRequest;
