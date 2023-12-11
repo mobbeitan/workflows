a8926b554ad6480e40f5186747f0a8d5ea7b57a7
Mobb autofixer <git@mobb.ai>
2023-12-11 16:50:30 +0000

    mobb fix commit: 046a362a-189f-437e-b07e-ac92213c149a

--- src/main/java/my naive db sample.java
+++ src/main/java/my naive db sample.java
@@ -1,3 +1,4 @@
+import java.sql.PreparedStatement;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,10 +13,15 @@ public class SQLInjectionExample extends HttpServlet {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db");
             String user = request.getParameter("username");
 
-            String query = "SELECT * FROM users WHERE username = '" + user + "';";
-            Statement stmt = con.createStatement();
+            String query = "SELECT * FROM users WHERE username = '?';";
+            PreparedStatement stmt = con.prepareStatement(query);
 
-            stmt.executeQuery(query);
+            try {
+                stmt.setInt(1, Math.round(Float.parseFloat(user)));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            stmt.executeQuery();
         } catch (Exception e) {
             throw new ServletException(e);
         }
