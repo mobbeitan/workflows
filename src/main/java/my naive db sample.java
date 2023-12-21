62b6e4c61946b9208a7c79520b01ac34dc01d6bf
Mobb-dev <94089762+Mobb-dev@users.noreply.github.com>
2023-12-21 11:51:20 -0500

    code changes

--- src/main/java/my naive db sample.java
+++ src/main/java/my naive db sample.java
@@ -6,14 +6,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+
 public class SQLInjectionExample extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db");
-            String user = request.getParameter("username");
+            String var = request.getParameter("username");
 
-            String query = "SELECT * FROM users WHERE username2 = '" + user + "';";
+            String query = "SELECT * FROM users WHERE username = '" + user + "';";
             Statement stmt = con.createStatement();
+
+
+            
+            String queryss = "string" + var;
+            String querys = "string" + var + "string";
+            String.format("string = '%s';", var);
+            "SELECT * FROM users WHERE username = '".concat(var).concat("';");
 
             stmt.executeQuery(query);
         } catch (Exception e) {
