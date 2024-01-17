15d20f47b8816308664d3bd0fd05e86b929ece3b
Eitan Worcel <96389636+mobbeitan@users.noreply.github.com>
2024-01-17 08:07:49 -0500

    Delete xss-example.js

--- xss-example.js
+++
@@ -1,5 +0,0 @@
-var urlParams = new URLSearchParams(window.location.search);
-var name = urlParams.get('name');
-
-var unsafe_div = window.document.getElementById("vulnerable-div");
-unsafe_div.innerHTML = "Hello " + name; 
